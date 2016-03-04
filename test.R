e5800e7528ca1a42135a3edb434a0b8c116bc6fe
Joon Kwon <joon.kwon@bellmedia.ca>
2016-03-04 17:15:09 -0500

    playing around

--- test.R
+++ test.R
@@ -22,7 +22,6 @@ storm$total.dmg <- storm$PROPDMG + storm$CROPDMG
 str(storm)
 
 library(dplyr)
-library(lattice)
 storm.by.year.evt <- group_by(storm, year, EVTYPE)
 sum.dmg.by.year.evt <- summarise(storm.by.year.evt, sum.dmg = sum(total.dmg))
 head(sum.dmg.by.year.evt)
