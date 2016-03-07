8f37ab1737d6ce3be5725dc76be6d630237ae703
Joon Kwon <joon.kwon@bellmedia.ca>
2016-03-07 16:50:50 -0500

    swapped health impact and economic impact sections

--- storm_health.R
+++ storm_health.R
@@ -26,41 +26,21 @@ str(storm)
 
 ## ----message=FALSE-------------------------------------------------------
 library(dplyr)
+storm <- tbl_df(storm)
 
-## ----cache=TRUE----------------------------------------------------------
+## ----cache=FALSE---------------------------------------------------------
 storm.by.year.evt <- group_by(storm, year, EVTYPE)
 storm.by.year.evt
 
-## ----cache=TRUE----------------------------------------------------------
-sum.dmg<- summarise(storm.by.year.evt, sum.dmg = sum(total.dmg))
-head(sum.dmg)
-dim(sum.dmg)
-length(unique(sum.dmg$EVTYPE))
-
-## ----cache=TRUE----------------------------------------------------------
-sum.dmg.by.evt <- group_by(sum.dmg, EVTYPE)
-mean.dmg.by.evt <- summarise(sum.dmg.by.evt, dmg = mean(sum.dmg))
-mean.dmg.by.evt <- mean.dmg.by.evt[order(mean.dmg.by.evt$dmg, decreasing = TRUE),]
-mean.dmg.by.evt
-
-## ----message=FALSE-------------------------------------------------------
-library(ggplot2)
-
-## ------------------------------------------------------------------------
-mean.dmg.top <- mean.dmg.by.evt[1:10,]
-mean.dmg.top$dmg <- mean.dmg.top$dmg / 1e6
-g <- ggplot(mean.dmg.top, aes(x=EVTYPE, y=dmg))
-g + geom_bar(stat="identity", fill="green") +  
-    theme(axis.text.x = element_text(angle = 90, hjust=1)) +
-    geom_text(aes(label=paste(sprintf( "%1.0f", dmg), "M" ), y=dmg + 700, colour = "pink" ),
-              size = 3, show.legend = FALSE) +
-    xlab("Event Type") +ylab("Cost of Damage (Million)")
-
-## ----cache=TRUE----------------------------------------------------------
+## ----cache=FALSE---------------------------------------------------------
 sum.inj.fat <- summarise(storm.by.year.evt, total.injuries=sum(INJURIES), total.fatalities = sum(FATALITIES))
 sum.inj.fat
 
-## ----cache=TRUE----------------------------------------------------------
+sum.dmg<- summarise(storm.by.year.evt, sum.dmg = sum(total.dmg))
+head(sum.dmg)
+
+
+## ----cache=FALSE---------------------------------------------------------
 inj.fat.by.event <- group_by(sum.inj.fat, EVTYPE)
 inj.fat.by.event
 mean.inj.fat <- summarise(inj.fat.by.event, mean.inj = mean(total.injuries), 
@@ -72,13 +52,13 @@ mean.inj.fat.sort.by.inj
 mean.inj.fat.sort.by.fat
 
 
-## ----cache=TRUE----------------------------------------------------------
+## ----cache=FALSE---------------------------------------------------------
 top.inj <- mean.inj.fat.sort.by.inj[1:10,]$EVTYPE
 top.fat <- mean.inj.fat.sort.by.fat[1:10,]$EVTYPE
 top.inj.fat.evt <- union(top.inj, top.fat)
 top.inj.fat.evt
 
-## ----cache=TRUE----------------------------------------------------------
+## ----cache=FALSE---------------------------------------------------------
 top.inj.fat <- mean.inj.fat[mean.inj.fat$EVTYPE %in% top.inj.fat.evt,]
 top.inj.fat
 
@@ -86,12 +66,15 @@ top.inj.fat
 library(tidyr)
 library(plyr)
 
-## ----cache=TRUE----------------------------------------------------------
+## ----cache=FALSE---------------------------------------------------------
 top.evt <- gather(top.inj.fat, "type", "count", 2:3)
 top.evt$type <- as.factor(top.evt$type)
 top.evt
 
-## ----cache=TRUE----------------------------------------------------------
+## ----message=FALSE-------------------------------------------------------
+library(ggplot2)
+
+## ----cache=FALSE---------------------------------------------------------
 g <- ggplot(top.evt, aes(EVTYPE,count,fill=type))
 g + theme(axis.text.x = element_text(angle = 90, hjust=1)) +
     geom_bar(stat="identity", position="dodge") +
@@ -99,16 +82,36 @@ g + theme(axis.text.x = element_text(angle = 90, hjust=1)) +
     scale_fill_discrete(name="", breaks=c("mean.fat", "mean.inj"), labels=c("fatalities","injuries")) +
     xlab("Event") + ylab("Mean Count")
 
-## ----cache=TRUE----------------------------------------------------------
+## ----cache=FALSE---------------------------------------------------------
 top.evt <- ddply(top.evt, "EVTYPE", transform, label_height = cumsum(count) - count / 2 )
 
-## ----cache=TRUE----------------------------------------------------------
+## ----cache=FALSE---------------------------------------------------------
 g <- ggplot(top.evt, aes(EVTYPE,count,fill=type))
 g + theme(axis.text.x = element_text(angle = 90, hjust=1)) +
     geom_bar(stat="identity", position="stack") +
     geom_text(aes(label=sprintf( "%1.0f", count), y = label_height), size=2.2) +
     scale_fill_discrete(name="", breaks=c("mean.fat", "mean.inj"), labels=c("fatalities","injuries")) +
     xlab("Event") + ylab("Mean Count")
+
+## ----cache=FALSE---------------------------------------------------------
+dim(sum.dmg)
+length(unique(sum.dmg$EVTYPE))
+
+## ----cache=FALSE---------------------------------------------------------
+sum.dmg.by.evt <- group_by(sum.dmg, EVTYPE)
+mean.dmg.by.evt <- summarise(sum.dmg.by.evt, dmg = mean(sum.dmg))
+mean.dmg.by.evt <- mean.dmg.by.evt[order(mean.dmg.by.evt$dmg, decreasing = TRUE),]
+mean.dmg.by.evt
+
+## ------------------------------------------------------------------------
+mean.dmg.top <- mean.dmg.by.evt[1:10,]
+mean.dmg.top$dmg <- mean.dmg.top$dmg / 1e6
+g <- ggplot(mean.dmg.top, aes(x=EVTYPE, y=dmg))
+g + geom_bar(stat="identity", fill="green") +  
+    theme(axis.text.x = element_text(angle = 90, hjust=1)) +
+    geom_text(aes(label=paste(sprintf( "%1.0f", dmg), "M" ), y=dmg + 700, colour = "pink" ),
+              size = 3, show.legend = FALSE) +
+    xlab("Event Type") +ylab("Cost of Damage (Million)")
 
 ## ------------------------------------------------------------------------
 sessionInfo()
